7d40bda11bd0f5e0f20cce6de44626eb9abc388d
Хитрин Влад <v.hitrin@gmail.com>
2017-02-07 00:17:14 +0300

    Create project

---
+++ app/src/main/java/ru/vhitrin/android/debts/DebtsActivity.java
@@ -0,0 +1,13 @@
+package ru.vhitrin.android.debts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DebtsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debts);
+    }
+}
